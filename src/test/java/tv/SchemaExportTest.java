1142dc1a3385766c4d8212b34f20ab0ed4976be3
Tomasz Pazurkiewicz <t.pazurkiewicz@gmail.com>
2011-10-29 01:06:09 +0200

    Change people schema.

---
+++ src/test/java/tv/SchemaExportTest.java
@@ -0,0 +1,21 @@
+package tv;
+
+import org.hibernate.cfg.Configuration;
+import org.hibernate.tool.hbm2ddl.SchemaExport;
+import org.junit.Test;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: tomek
+ * Date: 29.10.11
+ * Time: 00:55
+ * To change this template use File | Settings | File Templates.
+ */
+public class SchemaExportTest {
+    @Test
+    public void export(){
+//        SchemaExport se = new SchemaExport(new Configuration().configure());
+//        se.drop(true, false);
+//        se.create(true, false);
+    }
+}
